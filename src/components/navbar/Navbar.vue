1e1411e90385bc12996f2d8a90e421fb54d14ed9
murtazaaylak <murtazaaylak@gmail.com>
2021-11-17 15:22:34 +0300

    create: navbar component

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar" :class="{ 'navbar--dark': isDark }">
+    <router-link to="/" class="navbar__logo">Where in the world?</router-link>
+    <div class="navbar__theme">
+      <sun-icon />
+      <span>Light Mode</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SunIcon from '../icons/SunIcon.vue'
+export default {
+  components: { SunIcon },
+  name: 'Navbar',
+  data() {
+    return {
+      isDark: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss" src="./navbar.scss"></style>
