550c3412cc83446739d03dfe20ca469b0568a9cf
murtazaaylak <murtazaaylak@gmail.com>
2021-11-17 15:23:27 +0300

    refactor: add global styles for body

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+// import Navbar from './components/Navbar.vue'
+import Navbar from './components/navbar/Navbar.vue'
+export default {
+  name: 'App',
+  components: { Navbar },
+}
+</script>
 
 <style lang="scss">
 #app {
