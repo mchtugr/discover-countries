7bf6cc4bed13e86ab40716f8881e925e6e810232
murtazaaylak <murtazaaylak@gmail.com>
2021-11-18 21:19:45 +0300

    create: Grid Container component for country cards

---
+++ src/components/grid-container/GridContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid-container">
+    <country-card
+      v-for="i in 15"
+      :key="i"
+      :countryData="{
+        name: 'Argentina',
+        population: 123123123,
+        region: 'Americas',
+        capital: 'Buenos Aires',
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import CountryCard from '../country-card/CountryCard.vue'
+export default {
+  name: 'GridContainer',
+  components: { CountryCard },
+}
+</script>
+
+<style lang="scss" src="./grid-container.scss"></style>
