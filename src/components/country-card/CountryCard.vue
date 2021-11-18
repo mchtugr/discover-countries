dab03b4cd014105f9ea1dd599615c46690b009e6
murtazaaylak <murtazaaylak@gmail.com>
2021-11-18 20:43:59 +0300

    create: country card component

---
+++ src/components/country-card/CountryCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="country-card" :class="{ 'country-card--dark': isDark }">
+    <div class="country-card__imgbox">
+      <img
+        src="https://flagcdn.com/ar.svg"
+        class="country-card__img"
+        alt="flag"
+      />
+    </div>
+    <div class="country-card__body">
+      <h3 class="country-card__title">{{ countryData.name }}</h3>
+      <p>Population: {{ countryData.population }}</p>
+      <p>Region: {{ countryData.region }}</p>
+      <p>Capital: {{ countryData.capital }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'CountryCard',
+  props: {
+    countryData: Object,
+  },
+  computed: {
+    ...mapState(['isDark']),
+  },
+}
+</script>
+
+<style lang="scss" src="./country-card.scss"></style>
