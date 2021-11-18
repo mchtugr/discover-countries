fac4bd7880b7f6768a9521e1b616006d18856377
murtazaaylak <murtazaaylak@gmail.com>
2021-11-18 12:58:50 +0300

    create: select input component

---
+++ src/components/vue-select/VueSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="select-dropdown" :class="{ 'select-dropdown--dark': isDark }">
+    <vSelect :placeholder="placeholder" :options="options"></vSelect>
+  </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+import { mapState } from 'vuex'
+export default {
+  name: 'VueSelect',
+  components: { vSelect },
+  props: {
+    options: Array,
+    placeholder: String,
+  },
+  computed: {
+    ...mapState(['isDark']),
+  },
+}
+</script>
+
+<style lang="scss" src="./vue-select.scss"></style>
