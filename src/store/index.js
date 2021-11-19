510a43483675b0da14ce2521b1f8a901d65b7523
murtazaaylak <murtazaaylak@gmail.com>
2021-11-19 13:02:39 +0300

    add: fetch all countries via vuex action

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isDark: false,
+    isDark: true,
+    countries: [],
   },
   mutations: {
     SWITCH_THEME(state) {
       state.isDark = !state.isDark
     },
+    GET_ALL_COUNTRIES(state, payload) {
+      state.countries = payload
+    },
   },
   actions: {
     switchTheme({ commit }) {
       commit('SWITCH_THEME')
+    },
+    async getAllCountries({ commit }) {
+      try {
+        const response = await axios.get('https://restcountries.com/v2/all')
+        commit('GET_ALL_COUNTRIES', response.data)
+      } catch (error) {
+        console.log(error.message)
+      }
     },
   },
   modules: {},
