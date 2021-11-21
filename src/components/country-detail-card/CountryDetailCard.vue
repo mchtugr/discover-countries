fa30c1757997563ebab71802bd8bd5bfe38fa49d
murtazaaylak <murtazaaylak@gmail.com>
2021-11-21 20:41:25 +0300

    display country detail data dynamically via props

--- src/components/country-detail-card/CountryDetailCard.vue
+++ src/components/country-detail-card/CountryDetailCard.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="detail-card">
+  <div class="detail-card" v-if="countryData">
     <div class="detail-card__imgbox">
-      <img src="https://flagcdn.com/ar.svg" alt="" class="detail-card__img" />
+      <img
+        :src="countryData.flag"
+        :alt="countryData.name"
+        class="detail-card__img"
+      />
     </div>
     <div class="detail-card__body">
-      <div class="detail-card__title">Argentina</div>
+      <div class="detail-card__title">{{ countryData.name }}</div>
       <div
         class="detail-card__grid-container"
         :class="{ 'detail-card__grid-container--dark': isDark }"
@@ -12,46 +16,52 @@
         <div class="detail-card__left-grid">
           <li class="detail-card__list-item">
             <span class="boldish">Native Name:</span>
-            <span> {{ 'Argentina' }}</span>
+            <span> {{ countryData.nativeName }}</span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Population:</span>
-            <span> {{ 123123123 }}</span>
+            <span> {{ countryData.population }}</span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Region:</span>
-            <span> {{ 'Americas' }}</span>
+            <span> {{ countryData.region }}</span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Sub Region:</span>
-            <span> {{ 'South Americas' }}</span>
+            <span> {{ countryData.subregion }}</span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Capital:</span>
-            <span> {{ 'Buenos Aires' }}</span>
+            <span> {{ countryData.capital }}</span>
           </li>
         </div>
         <div class="detail-card__right-grid">
           <li class="detail-card__list-item">
             <span class="boldish">Top Level Domain:</span>
-            <span> {{ 'ar' }} </span>
+            <span> {{ countryData.topLevelDomain[0] }} </span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Currencies:</span>
-            <span> {{ 'Peso' }}</span>
+            <span>
+              {{ currencies }}
+            </span>
           </li>
           <li class="detail-card__list-item">
             <span class="boldish">Languages:</span>
-            <span> {{ 'Spanish, French, German' }}</span>
+            <span> {{ languages }}</span>
           </li>
         </div>
       </div>
       <div class="detail-card__footer">
-        <span class="boldish">Border Countries:</span>
+        <div class="boldish">Border Countries:</div>
         <div class="detail-card__border-countries">
-          <div class="detail-card__border-country">Brazil</div>
-          <div class="detail-card__border-country">Brazil</div>
-          <div class="detail-card__border-country">Brazil</div>
+          <div
+            class="detail-card__border-country"
+            v-for="(bc, i) in borderCountries"
+            :key="i"
+          >
+            {{ bc }}
+          </div>
         </div>
       </div>
     </div>
@@ -62,8 +72,34 @@
 import { mapState } from 'vuex'
 export default {
   name: 'CountryDetailCard',
+  props: {
+    countryData: Object,
+  },
   computed: {
-    ...mapState(['isDark']),
+    ...mapState(['isDark', 'countries']),
+    // returns currencies arr as comma seperated string
+    currencies() {
+      return this.countryData.currencies
+        .map((currency) => currency.name)
+        .join(', ')
+    },
+    // returns languages arr as comma seperated string
+    languages() {
+      return this.countryData.languages
+        .map((language) => language.name)
+        .join(', ')
+    },
+    // map border country codes as full name
+    borderCountries() {
+      const neighbors = []
+      this.countryData.borders.forEach((countryCode) => {
+        const newNeighbor = this.countries.find(
+          (country) => country.alpha3Code === countryCode
+        )
+        neighbors.push(newNeighbor.name)
+      })
+      return neighbors
+    },
   },
 }
 </script>
